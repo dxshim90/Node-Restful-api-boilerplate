c0d293a36728d1e6e728deab4b32921341128716
github username <github email address>
2019-07-02 20:15:29 +0100

    Product routes done

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,48 +1,104 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const Product = require('../models/product')
 
 
 
 //url = /products
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET Request to /products'
+    Product.find().exec()
+    .then(docs => {
+        console.log(docs)
+        res.status(200).json(docs)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: err
+        })
     })
 })
 
 
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling POST Request to /products'
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    })
+    product.save().then(result => {
+        console.log(result)
+        res.status(200).json({
+            message: 'Handling POST Request to /products',
+            createdProduct: product
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
+    })
+
+    
     })
 })
 
 router.get('/:id', (req, res, next) => {
     const id = req.params.id
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You Discovered The Special ID',
-            id: id
+    Product.findById(id).exec().then(doc => {
+        console.log(doc)
+        if (!doc) {
+            res.status(500).json({
+                message: 'No Such Product'
+            })
+        }
+        res.status(200).json(doc)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
         })
-    } else {
-        res.status(200).json({
-            message: 'you passed an id'
-        })
-    }
+    })
 })
 
 router.patch('/:id', (req, res, next) => {
-res.status(200).json({
-    message: 'Updated Product'
-})
+    const id = req.params.id
+    const updateOps = {}
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
+    }
+    Product.update({_id: id}, {$set: updateOps})
+    .exec()
+    .then(result => {
+        console.log(result)
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
+    })
 })
 
-router.delete('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'product Deleted'
-    })
+router.delete('/:id', async (req, res, next) => {
+        const id = req.params.id
+        try {
+           await Product.deleteOne({_id: id})
+           res.status(500).json({
+               message: 'Product Removed'
+           })
+           
+        } catch (err) {
+            res.status(500).json({
+                error: err
+            })
+        }
     })
 
 
--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
+mongoose.connect('mongodb+srv://Danny123:Danny123@cluster0-8nih0.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})
+
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next()
+})
 
 
 app.use('/products', productRoutes)
